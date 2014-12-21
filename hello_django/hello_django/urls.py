144cd393687212aa2d252bae110080b5f017a5c3
Antoine Menini <antoinemenini@gmail.com>
2014-12-21 12:41:21 +0000

    hello world

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hello_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('main.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
